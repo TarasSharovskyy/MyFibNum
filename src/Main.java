00bb4f0f3296f3fdd42b0753bb18797389cbf2f6
TarasSharovskyy <t.sharovskyy@gmail.com>
2017-08-07 19:36:25 +0300

    myBigIntegerFibonacciNum

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,62 @@
 import java.math.BigInteger;
 import java.util.Scanner;
-
 public class Main {
-
-    public long n;
-    public long i;
-
-
-    public static BigInteger fibonacci2(long n) {
-
-        BigInteger a = BigInteger.valueOf(0);
-        BigInteger b = BigInteger.valueOf(1);
-        BigInteger c = BigInteger.valueOf(1);
-        for (int j=2 ; j<=n ; j++)
-        {
-            c =  a.add(b);
-            a = b;
-            b = c;
+    public int n;
+    public static int u;
+    public double isEven = 0;
+    public double isOdd = 0;
+    public static BigInteger fibonacci2(int n) {
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ONE;
+        if (n<2){
+            return a;
         }
-
-        return (a);
-
+        BigInteger c = null;
+        while(n-- >=2){
+            c=a.add(b);
+            a=b;
+            b=c;
+        }
+        return c;
     }
-
+    public static void myElement(int u) {
+        System.out.println("element #" + u + " " + fibonacci2(u - 1));
+    }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner sc = new Scanner(System.in);
-
         do {
             System.out.println("Please enter a positive number: ");
-            while(!sc.hasNextLong()){
+            while (!sc.hasNextInt()) {
                 System.out.println("That`s not a number!");
                 System.out.println("Please enter a positive number: ");
                 sc.next();
             }
-            main.n = sc.nextLong();
+            main.n = sc.nextInt();
         } while (main.n <= 0);
-
-        for (long i = 0; i < main.n; i++) {
-            System.out.println(i+" - " +fibonacci2(i));
+        for (int i = 0; i < main.n; i++) {
+            System.out.println(i + " - " + fibonacci2(i));
+            if (fibonacci2(i).testBit(1)) {
+                main.isEven++;
+            } else {
+                main.isOdd++;
+            }
         }
+        System.out.println(" ");
+        System.out.println("even - " + ((main.isEven / main.n) * 100) + "%");
+        System.out.println("odd - " + ((main.isOdd / main.n) * 100) + "%");
+        System.out.println(" ");
+        Scanner sc2 = new Scanner(System.in);
+        do {
 
+            System.out.println("Please enter a number FROM 1 to " + main.n);
+            while (!sc2.hasNextInt()) {
+                System.out.println("That`s not a number!");
+                System.out.println("Please enter a positive number: ");
+                sc2.next();
+            }
+            main.u = sc2.nextInt();
+        } while (main.u <= 0 || main.u > main.n);
+        myElement(u);
     }
 }
